a7ef4a80820f3864defe596d29683ccb600eb465
hanjung <hanjung@linecorp.com>
2024-10-26 20:32:57 +0900

    week 7 solution

---
+++ server.js
@@ -0,0 +1,77 @@
+import express from "express";
+import fs from "fs";
+import { readFile } from "fs/promises";
+import path from "path";
+
+const app = express();
+const port = 3000;
+const __dirname = path.resolve();
+
+app.use(express.json());
+
+const getEvents = async () => {
+  const data = await readFile(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    "utf8"
+  );
+
+  return JSON.parse(data);
+};
+
+app.get("/api/events", async (_, res) => {
+  const events = await getEvents();
+  res.json(events);
+});
+
+app.post("/api/events", async (req, res) => {
+  const events = await getEvents();
+  const newEvent = { id: Date.now(), ...req.body };
+
+  fs.writeFileSync(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    JSON.stringify({
+      events: [...events.events, newEvent],
+    })
+  );
+
+  res.status(201).json(newEvent);
+});
+
+app.put("/api/events/:id", async (req, res) => {
+  const events = await getEvents();
+  const id = parseInt(req.params.id);
+  const eventIndex = events.events.findIndex((event) => event.id === id);
+  if (eventIndex > -1) {
+    const newEvents = [...events.events];
+    newEvents[eventIndex] = { ...events.events[eventIndex], ...req.body };
+
+    fs.writeFileSync(
+      `${__dirname}/src/__mocks__/response/realEvents.json`,
+      JSON.stringify({
+        events: newEvents,
+      })
+    );
+
+    res.json(events.events[eventIndex]);
+  } else {
+    res.status(404).send("Event not found");
+  }
+});
+
+app.delete("/api/events/:id", async (req, res) => {
+  const events = await getEvents();
+  const id = parseInt(req.params.id);
+
+  fs.writeFileSync(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    JSON.stringify({
+      events: events.events.filter((event) => event.id !== id),
+    })
+  );
+
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
